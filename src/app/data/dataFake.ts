14c67d4dbfd225b7f08628e7bd44876edb8b1559
Murilo Santos <muriloscunha23@gmail.com>
2023-08-16 08:31:06 -0300

    refactored code

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,13 +2,13 @@ export const dataFake = [
   {
     "id": "1",
     "title": "Deyverson: o pai do Flamengo",
-    "description": "ola",
+    "description": "Deyverson sem comentários, craque de bola",
     "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQftUZzsd5d4aIIoSQBFxAxeXRxvSSQdLRpuw&usqp=CAU"
   },
   {
     "id": "2",
     "title": "Painato Augusto: o craque do timão",
-    "description": "ola",
+    "description": "Conheça Renato Augusto, vovô do timão mas melhor que muitos jogadores profissionais",
     "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdF1ETI_DDDYAiDSAeiw6i4e8CAV-oqCQ6Yw&usqp=CAU"
   }
 ]
