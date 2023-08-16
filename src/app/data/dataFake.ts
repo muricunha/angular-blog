6a7a7834d6424e8c8820e54ade8e768ff0a37a78
Murilo Santos <muriloscunha23@gmail.com>
2023-08-16 08:20:45 -0300

    added router

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Deyverson: o pai do Flamengo",
+    "description": "ola",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQftUZzsd5d4aIIoSQBFxAxeXRxvSSQdLRpuw&usqp=CAU"
+  },
+  {
+    "id": "2",
+    "title": "Painato Augusto: o craque do timão",
+    "description": "ola",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdF1ETI_DDDYAiDSAeiw6i4e8CAV-oqCQ6Yw&usqp=CAU"
+  }
+]
